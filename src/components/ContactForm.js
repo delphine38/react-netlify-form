2d3ccabf25e1af40d29dc6889b5cf8c5af7422a8
delphine <delphin38@live.fr>
2022-05-28 20:39:48 +0200

    add contact

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -4,8 +4,23 @@ import "./ContactForm.css";
 
 function ContactForm() {
   return (
-    <div>
-      form
+    <div className="contact">
+      <form name="contact" 
+            method="post" 
+            className='contact_form' 
+            data-netlify="true" 
+            onSubmit="submit">
+
+            <input type="hidden" name="form-name" value="contact" />
+            <input type="text" name="name" placeholder='Nom'/>
+          
+            <input type="email" name="email" placeholder='Email'/>
+         
+            <textarea name="message" placeholder='Message'></textarea>
+         
+            <button type="submit">Envoyer</button>
+         
+        </form>
     </div>
   )
 }
