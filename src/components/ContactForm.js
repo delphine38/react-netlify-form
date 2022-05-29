d1f5ec7d28591e783e6b1e5cf3e10b744885692f
delphine <delphin38@live.fr>
2022-05-29 08:38:33 +0200

    update

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -12,11 +12,11 @@ function ContactForm() {
             onSubmit="submit">
 
             <input type="hidden" name="form-name" value="contact" />
-            <input type="text" name="name" placeholder='Nom'/>
+            <input type="text" name="name" placeholder='Votre nom, prénom'/>
           
-            <input type="email" name="email" placeholder='Email'/>
+            <input type="email" name="email" placeholder='Votre email'/>
          
-            <textarea name="message" placeholder='Message'></textarea>
+            <textarea name="message" placeholder='Votre message'></textarea>
          
             <button type="submit">Envoyer</button>
          
